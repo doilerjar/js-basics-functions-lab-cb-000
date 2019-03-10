cac28e9062f0ab76618ab1006aaf7c8afe7c3162
Kevin K <doilerjar@outlook.com>
2019-03-10 16:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
 function distanceTravelledInFeet(beginBlock, endBlock) {
+  distance = endBlock - beginBlock;
+  if (distance < 0) {
+    distance = -distance;
+  }
   return (endBlock-beginBlock)*264;
 }
