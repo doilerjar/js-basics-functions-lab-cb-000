53da1333be224c19b6b2b4e6003b42ce5f1d9585
Kevin K <doilerjar@outlook.com>
2019-03-10 19:03:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function distanceFromHqInFeet(value) {
 function distanceFromHqInBlocks(value) {
   return Math.abs(value-42);
 }
+
+function calculatesFarePrice(start, destination) {
+  distance = distanceFromHqInFeet(Math.abs(destination - start));
+  if (distance <= 400) {
+    return 0;
+  }
+  else if (distance > 400 && distance <= 2000) {
+    return distance * 0.02;
+  }
+  else if (distancae > 2000 && distance <= 2500) {
+    return 25;
+  }
+}
